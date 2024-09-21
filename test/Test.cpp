16a255ff25d919fe124b080846cc78767b065757
AnttoLion <anttolion@gmail.com>
2024-09-21 15:42:56 -0400

    init

---
+++ test/Test.cpp
@@ -0,0 +1,8 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch_all.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+TEST_CASE("Example Test Case") {
+    REQUIRE(1 + 1 == 2);
+    REQUIRE(1 + 2 == 3);
+}
